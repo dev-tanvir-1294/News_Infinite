dcf6814ff9a7b90f9d72174878a0d790cbfe85ca
dev-tanvir-1294 <tanvirahmed1294@gmail.com>
2025-07-31 16:23:02 +0600

    new commit from visual code

--- admin/add-post.php
+++ admin/add-post.php
@@ -11,7 +11,7 @@
 
 
                 <!-- Form -->
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="save-post.php" method="POST" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="post_title">Title</label>
                         <input type="text" name="post_title" class="form-control" autocomplete="off" required>
@@ -23,7 +23,27 @@
                     <div class="form-group">
                         <label for="exampleInputPassword1">Category</label>
                         <select name="category" class="form-control">
-                            <option value="" selected> Select Category</option>
+                            <option disable> Select Category</option>
+
+                            <?php
+
+                            include "config.php";
+                            $sql = "SELECT * FROM category";
+
+                            $result = mysqli_query($conn, $sql) or die("Error in query: " . mysqli_error($conn));
+
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+
+                                    echo "<option value={$row['category_id']}>{$row['category_name']}</option>";
+                                }
+                            }
+
+
+
+
+                            ?>
+
                         </select>
                     </div>
                     <div class="form-group">
